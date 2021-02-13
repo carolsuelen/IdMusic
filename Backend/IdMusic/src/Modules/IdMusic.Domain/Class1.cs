5995125c51f910a6e6809fb0006f92d653e02ded
Rachel Kozlowsky <rachelkozlowsky@gmail.com>
2021-02-13 12:15:05 -0300

    tables modifications

---
+++ Backend/IdMusic/src/Modules/IdMusic.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IdMusic.Domain
+{
+    public class Class1
+    {
+    }
+}
--- Backend/sql/bd.sql
+++ Backend/sql/bd.sql
@@ -1,95 +1,95 @@
-CREATE DATABASE Instagama;
-USE Instagama;
+CREATE DATABASE IdMusic;
+USE IdMusic;
 --Criação do Database
 
 
 --Criação das tabelas
-CREATE TABLE dbo.Genero (
+CREATE TABLE dbo.Genre (
    Id int IDENTITY(1,1) NOT NULL,
-   Descricao varchar(50) NOT NULL,
-   CONSTRAINT PK_Genero_Id PRIMARY KEY CLUSTERED (Id)
+   Description varchar(50) NOT NULL,
+   CONSTRAINT PK_Genre_Id PRIMARY KEY CLUSTERED (Id)
 )
 
-CREATE TABLE dbo.Usuario (
+CREATE TABLE dbo.Client (
 	Id int IDENTITY(1,1) NOT NULL,
-	GeneroId int NOT NULL,
-	Nome varchar(250) NOT NULL,
+	GenreId int NOT NULL,
+	Name varchar(250) NOT NULL,
 	Email varchar(100) NOT NULL,
-	Senha varchar(200) NOT NULL,
-	DataNascimento DateTime NOT NULL,
-	Foto varchar(max) NOT NULL,
-	FotoCapa varchar (max) NOT NULL,
-	Biografia varchar (500) NOT NULL,
-	Banda varchar (max) NOT NULL 
-	CONSTRAINT PK_Usuario_Id PRIMARY KEY CLUSTERED (Id)
+	Password varchar(200) NOT NULL,
+	Birthday DateTime NOT NULL,
+	Photo varchar(max) NOT NULL,
+	PhotoCapa varchar (max) NOT NULL,
+	Biografy varchar (500) NOT NULL,
+	Band varchar (max) NOT NULL 
+	CONSTRAINT PK_Client_Id PRIMARY KEY CLUSTERED (Id)
 )
 
-ALTER TABLE dbo.Usuario
-   ADD CONSTRAINT FK_Usuario_Genero FOREIGN KEY (GeneroId)
-      REFERENCES dbo.Genero (Id)
+ALTER TABLE dbo.Client
+   ADD CONSTRAINT FK_Client_Genre FOREIGN KEY (GenreId)
+      REFERENCES dbo.Genre (Id)
 
 
-CREATE TABLE dbo.Postagem (
+CREATE TABLE dbo.Postage (
    Id int IDENTITY(1,1) NOT NULL,
-   UsuarioId int NOT NULL,
-   Texto varchar(250) NOT NULL,
-   Criacao DateTime NOT NULL,
-   CONSTRAINT PK_Postagem_Id PRIMARY KEY CLUSTERED (Id)
+   ClientId int NOT NULL,
+   Text varchar(250) NOT NULL,
+   Creation DateTime NOT NULL,
+   CONSTRAINT PK_Postage_Id PRIMARY KEY CLUSTERED (Id)
 )
 
-ALTER TABLE dbo.Postagem
-   ADD CONSTRAINT FK_Postagem_Usuario FOREIGN KEY (UsuarioId)
-      REFERENCES dbo.Usuario (Id)
+ALTER TABLE dbo.Postage
+   ADD CONSTRAINT FK_Postage_Client FOREIGN KEY (ClientId)
+      REFERENCES dbo.Client (Id)
 	  
 	  
-CREATE TABLE dbo.Comentario (
+CREATE TABLE dbo.Commentary (
    Id int IDENTITY(1,1) NOT NULL,
-   UsuarioId int NOT NULL,
-   PostagemId int NOT NULL,
-   Texto varchar(250) NOT NULL,
-   Criacao DateTime NOT NULL,
-   CONSTRAINT PK_Comentario_Id PRIMARY KEY CLUSTERED (Id)
+   ClientId int NOT NULL,
+   PostageId int NOT NULL,
+   Text varchar(250) NOT NULL,
+   Creation DateTime NOT NULL,
+   CONSTRAINT PK_Commentary_Id PRIMARY KEY CLUSTERED (Id)
 )
 
-ALTER TABLE dbo.Comentario
-   ADD CONSTRAINT FK_Comentario_Usuario FOREIGN KEY (UsuarioId)
-      REFERENCES dbo.Usuario (Id)
+ALTER TABLE dbo.Commentary
+   ADD CONSTRAINT FK_Commentary_Client FOREIGN KEY (ClientId)
+      REFERENCES dbo.Client (Id)
 	  
-ALTER TABLE dbo.Comentario
-   ADD CONSTRAINT FK_Comentario_Postagem FOREIGN KEY (PostagemId)
-      REFERENCES dbo.Postagem (Id)
+ALTER TABLE dbo.Commentary
+   ADD CONSTRAINT FK_Commentary_Postage FOREIGN KEY (PostageId)
+      REFERENCES dbo.Postage (Id)
 	  
-CREATE TABLE dbo.Curtidas (
+CREATE TABLE dbo.Likes (
    Id int IDENTITY(1,1) NOT NULL,
-   UsuarioId int NOT NULL,
-   PostagemId int NOT NULL,
-   CONSTRAINT PK_Curtidas_Id PRIMARY KEY CLUSTERED (Id)
+   ClientId int NOT NULL,
+   PostageId int NOT NULL,
+   CONSTRAINT PK_Likes_Id PRIMARY KEY CLUSTERED (Id)
 )
 
-ALTER TABLE dbo.Curtidas
-   ADD CONSTRAINT FK_Curtidas_Usuario FOREIGN KEY (UsuarioId)
-      REFERENCES dbo.Usuario (Id)
+ALTER TABLE dbo.Likes
+   ADD CONSTRAINT FK_Likes_Client FOREIGN KEY (ClientId)
+      REFERENCES dbo.Client (Id)
 	  
-ALTER TABLE dbo.Curtidas
-   ADD CONSTRAINT FK_Curtidas_Postagem FOREIGN KEY (PostagemId)
-      REFERENCES dbo.Postagem (Id)
+ALTER TABLE dbo.Likes
+   ADD CONSTRAINT FK_Likes_Postage FOREIGN KEY (PostageId)
+      REFERENCES dbo.Postage (Id)
 
-CREATE TABLE dbo.Amigos (
+CREATE TABLE dbo.Friends (
    Id int IDENTITY(1,1) NOT NULL,
-   UsuarioId int NOT NULL,
-   AmigoId int NOT NULL,
-   CONSTRAINT PK_Amigos_Id PRIMARY KEY CLUSTERED (Id)
+   ClientId int NOT NULL,
+   FriendId int NOT NULL,
+   CONSTRAINT PK_Friends_Id PRIMARY KEY CLUSTERED (Id)
 )
 
-ALTER TABLE dbo.Amigos
+ALTER TABLE dbo.Friends
    Id int IDENTITY(1,1) NOT NULL,
-   UsuarioId int NOT NULL,
-   UsuarioAmigoId int NOT NULL,
-   CONSTRAINT PK_Amigos_Id PRIMARY KEY CLUSTERED(Id),
-	CONSTRAINT FK_Amigos_Usuario_Amigo FOREIGN KEY(UsuarioAmigoId)
-		REFERENCES dbo.Usuario(Id),
-	CONSTRAINT FK_Amigos_Usuario FOREIGN KEY(UsuarioId)
-		REFERENCES dbo.Usuario (Id)
+   ClientId int NOT NULL,
+   ClientFriendId int NOT NULL,
+   CONSTRAINT PK_Friends_Id PRIMARY KEY CLUSTERED(Id),
+	CONSTRAINT FK_Friends_Client_Friend FOREIGN KEY(ClientFriendId)
+		REFERENCES dbo.Client(Id),
+	CONSTRAINT FK_Friends_Client FOREIGN KEY(ClientId)
+		REFERENCES dbo.Client (Id)
 
 
 
