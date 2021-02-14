4c8bcaaa334dc6cd64fdbd84161a7b5320b84700
Rachel Kozlowsky <rachelkozlowsky@gmail.com>
2021-02-13 21:22:10 -0300

    created bd azure, client and genre

--- Backend/IdMusic/src/Modules/IdMusic.Repositories/ClientRepository.cs
+++ Backend/IdMusic/src/Modules/IdMusic.Repositories/ClientRepository.cs
@@ -18,9 +18,59 @@ namespace IdMusic.Repositories
       _configuration = configuration;
     }
 
-    public Task<Client> GetByIdAsync(int id)
+    public async Task<Client> GetByIdAsync(int id)
     {
-      throw new NotImplementedException();
+      using (var con = new SqlConnection(_configuration["ConnectionString"]))
+      {
+        var sqlCmd = @$"SELECT u.Id,
+	                             u.Name,
+	                             u.Email,
+	                             u.Password,
+                               u.Birthday,
+                               u.Photo,
+                               u.PhotoCapa,
+                               u.Biografy,
+                               u.Band,
+	                             g.Id as GenreId,
+	                             g.Description
+                        FROM
+                              Usuario u
+                          INNER JOIN 
+	                            Genero g ON g.Id = u.GeneroId
+                          WHERE 
+	                            u.Id= '{id}'";
+
+        using (var cmd = new SqlCommand(sqlCmd, con))
+        {
+          cmd.CommandType = CommandType.Text;
+          con.Open();
+
+          var reader = await cmd
+                              .ExecuteReaderAsync()
+                              .ConfigureAwait(false);
+
+          while (reader.Read())
+          {
+
+            var client = new Client(reader["Name"].ToString(),
+                                      DateTime.Parse(reader["Birthday"].ToString()),
+                                      new Genre(reader["Description"].ToString()),
+                                      reader["Photo"].ToString(),
+                                      reader["PhotoCapa"].ToString(),
+                                      reader["Biografy"].ToString(),
+                                      reader["Band"].ToString());
+
+
+            client.InformationLoginClient(reader["Email"].ToString(), reader["Password"].ToString());
+            client.SetId(int.Parse(reader["id"].ToString()));
+            client.Genre.SetId(int.Parse(reader["GenreId"].ToString()));
+
+            return client;
+          }
+
+          return default;
+        }
+      }
     }
 
     public async Task<Client> GetByLoginAsync(string login)
