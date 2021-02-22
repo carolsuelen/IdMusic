4d01a0c8346e13c5d0329514587330e3bab80a0b
Rachel Kozlowsky <rachelkozlowsky@gmail.com>
2021-02-21 22:45:46 -0300

    k
    k

--- Backend/IdMusic/src/Modules/IdMusic.Application/AppClient/input/LoginInput.cs
+++
@@ -1,8 +0,0 @@
-namespace IdMusic.Application.AppClient.input
-{
-    public class LoginInput
-    {
-        public string Login { get; set; }
-        public string Password { get; set; }
-    }
-}
